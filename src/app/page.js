448a0ead7230c42871f4df145dc7353b34c5d772
Oscar Bertel P <osber00@gmail.com>
2024-01-20 10:08:59 -0500

    Ajustes en page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ const HomePage = async () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
       {tareas.map(tarea => (
-        <TareaCard tarea={tarea}/>
+        <TareaCard tarea={tarea} key={tarea.id}/>
       ))}
     </div>
   );
